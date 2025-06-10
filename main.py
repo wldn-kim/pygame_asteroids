bc11f98ef7695aa4b76acdf79e2f25babb7a8dd4
wldn-kim <logdotcom1234@gmail.com>
2025-06-10 12:24:59 -0400

    made blank pygame gui and you can X out of the game

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        return
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
