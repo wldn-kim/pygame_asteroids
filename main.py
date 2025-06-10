0a3c8ca80ffdc30f2263ab464277c5ebe3ca9857
wldn-kim <logdotcom1234@gmail.com>
2025-06-10 14:49:47 -0400

    grouped player functions

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while True:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        
+        for obj in drawable:
+            obj.draw(screen)
 
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+pygame.quit()
 if __name__ == "__main__":
     main()
